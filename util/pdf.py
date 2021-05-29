e5dc4f1ac0000c8943b98fe10cefe8ce8c8d146f
Christian Franchin dos Santos <christianfranchin@gmail.com>
2021-05-29 03:01:29 -0300

    PdfUtils to convert all images in a folder to pdf
    Signed-off-by: Christian Franchin dos Santos <christianfranchin@gmail.com>

--- util/pdf.py
+++ util/pdf.py
@@ -1,10 +1,21 @@
 import os
+from PIL import Image
 
-from PyPDF2 import PdfFileWriter
 
 class PdfUtils():
-    def __init__(self) -> None:
-        pass
-
+    @staticmethod
     def convert_folder_to_pdf(folder_path: str) -> str:
-        for root, dirs, files in os.walk(folder_path):
+        files_list = None
+        for root, _, files in os.walk(folder_path):
+            files_list = [os.path.join(root, name) for name in files]
+            files_list.sort()
+
+        image_list = [Image.open(im).convert('RGB') for im in files_list]
+
+        if folder_path.endswith('/'):
+            pdf_path = folder_path[:-1]
+        else:
+            pdf_path = folder_path
+        pdf_path = f'{pdf_path}.pdf'
+
+        image_list[0].save(pdf_path, save_all=True, append_images=image_list[1:])
