e43ef03e1b3ad2c522c7d8f2c572bf220449bcf7
Christian Franchin dos Santos <christianfranchin@gmail.com>
2021-05-29 03:28:47 -0300

    Reduce complexity of main
    Signed-off-by: Christian Franchin dos Santos <christianfranchin@gmail.com>

--- manga_downloader.py
+++ manga_downloader.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import typing
+from util.manga import Manga
 
 from util.utils import FormatText, dynamic_pad
 from util.pdf import PdfUtils
@@ -26,6 +27,58 @@ def parse_chapter_selection(selection: str) -> typing.List[int]:
             chapters = chapters + [int(section_range[0])]
 
     return chapters
+
+
+def chose_manga(provider: MangaHost):
+    chosen_manga = None
+    for manga in provider.find_mangas(args.manga):
+        manga.show()
+        response = None
+        while response not in ('S', 'N'):
+            response = input(FormatText.option('Deseja baixar este manga? S/N  ')).upper()
+
+        if response == 'N':
+            continue
+        elif response == 'S':
+            chosen_manga = manga
+            break
+
+    if chosen_manga is None:
+        print("O seu manga desejado não foi encontrado")
+        quit(0)
+    return manga
+
+
+def select_chapters(provider: MangaHost, manga: Manga):
+    chapters = provider.find_manga_chapters(manga)
+
+    for index, elem in enumerate(chapters, 1):
+        print(('{} - Capítulo #{}').format(dynamic_pad(len(chapters), index), elem))
+
+    response = input(FormatText.option('Quais indices deseja baixar?  '))
+    selected_chapters = parse_chapter_selection(response)
+    return selected_chapters, chapters
+
+
+def download_chapters(provider: MangaHost, manga: Manga, selected_chapters, chapters):
+    all_folders = []
+    for chapter in selected_chapters:
+        folder, _ = provider.download_chapter(manga, chapters[chapter - 1])
+        all_folders.append(folder)
+    return all_folders
+
+
+def manga_downloader(args: dict):
+    provider = MangaHost()
+
+    manga = chose_manga(provider)
+
+    selected_chapters, chapters = select_chapters(provider, manga)
+
+    all_folders = download_chapters(provider, manga, selected_chapters, chapters)
+
+    if not args.image:
+        _ = PdfUtils.convert_multiple_folders_to_pdf(all_folders)
 
 
 if __name__ == '__main__':
@@ -54,37 +107,4 @@ if __name__ == '__main__':
     else:
         logger.setLevel(logging.INFO)
 
-    provider = MangaHost()
-
-    chosen_manga = None
-    for manga in provider.find_mangas(args.manga):
-        manga.show()
-        response = None
-        while response not in ('S', 'N'):
-            response = input(FormatText.option('Deseja baixar este manga? S/N  ')).upper()
-
-        if response == 'N':
-            continue
-        elif response == 'S':
-            chosen_manga = manga
-            break
-
-    if chosen_manga is None:
-        print("O seu manga desejado não foi encontrado")
-        quit(0)
-
-    chapters = provider.find_manga_chapters(manga)
-
-    for index, elem in enumerate(chapters, 1):
-        print(('{} - Capítulo #{}').format(dynamic_pad(len(chapters), index), elem))
-
-    response = input(FormatText.option('Quais indices deseja baixar?  '))
-    selected_chapters = parse_chapter_selection(response)
-
-    all_folders = []
-    for chapter in selected_chapters:
-        folder, _ = provider.download_chapter(manga, chapters[chapter - 1])
-        all_folders.append(folder)
-
-    if not args.image:
-        result_files = PdfUtils.convert_multiple_folders_to_pdf(all_folders)
+    manga_downloader(args)
